52e9e2e5d519b332c50c6a98307536d7154e5b86
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2018-03-29 03:10:38 +0700

    update gen รูปได้แล้ว

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Cat from './Cat'
 import axios from 'axios'
+import HashImage from './HashImage'
+import crypto from 'crypto';
+
 class App extends Component {
   constructor(){
     super()
     this.state = {
       cats:[]
     }
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    this.hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
   }
   componentDidMount(){
     axios.get('http://www.mocky.io/v2/5ab7e14e3400007a0067ac75')
@@ -18,14 +24,19 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="columns is-multiline">
-        {
-          this.state.cats.map((cat)=>{
-            return <Cat key={cat.id} cat={cat}/>
-          })
-        } 
+      <div>
+        <HashImage hash={this.hash}/>
+        <br/>
+        <div className="columns is-multiline">
+          {
+            this.state.cats.map((cat)=>{
+              return <Cat key={cat.id} cat={cat}/>
+            })
+          } 
+        </div>
       </div>
     );
+
   }
 }
 
---
+++ src/HashImage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './HashImage.css'
+
+class HashImage extends Component {
+    sumHash(start){
+        var sum = 0;
+        this.props.hash.substring(start, 8).toUpperCase().split('').forEach((alphabet)=>{
+            sum += alphabet.charCodeAt(0);
+        });
+        return sum
+    }
+    render() {
+        return (
+            <div className="hash-image">
+                <div
+                    className="hash-image-accessories"
+                    style={{ backgroundImage: `url(${require(`./image/accessories/${this.sumHash(0) %16}.png`)})` }}>
+                </div>
+                <div
+                    className="hash-image-body"
+                    style={{ backgroundImage: `url(${require(`./image/body/${this.sumHash(8)%15}.png`)})` }}>
+                </div>
+                <div
+                    className="hash-image-top"
+                    style={{ backgroundImage: `url(${require(`./image/eyes/${this.sumHash(16)%15}.png`)})` }}>
+                </div>
+                <div
+                    className="hash-image-fur"
+                    style={{ backgroundImage: `url(${require(`./image/fur/${this.sumHash(24)%10}.png`)})` }}>
+                </div>
+                <div
+                    className="hash-image-top"
+                    style={{ backgroundImage: `url(${require(`./image/mouth/${this.sumHash(32)%5}.png`)})` }}>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default HashImage;
