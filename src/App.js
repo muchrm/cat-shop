2c264ad5288dc112776a853e9b696646ee51aef1
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2018-03-29 21:27:06 +0700

    เอา สคริปข้างนอกออก

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Cat from './Cat'
-import axios from 'axios'
+// import axios from 'axios'
 import HashImage from './HashImage'
 import crypto from 'crypto';
 
@@ -14,26 +14,26 @@ class App extends Component {
     var random = Math.random().toString();
     this.hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
   }
-  componentDidMount(){
-    axios.get('http://www.mocky.io/v2/5ab7e14e3400007a0067ac75')
-    .then((response) => {
-      this.setState({
-        cats:response.data
-      });
-    });
-  }
+  // componentDidMount(){
+  //   axios.get('http://www.mocky.io/v2/5ab7e14e3400007a0067ac75')
+  //   .then((response) => {
+  //     this.setState({
+  //       cats:response.data
+  //     });
+  //   });
+  // }
   render() {
     return (
       <div>
         <HashImage hash={this.hash}/>
-        <br/>
+        {/* <br/>
         <div className="columns is-multiline">
           {
             this.state.cats.map((cat)=>{
               return <Cat key={cat.id} cat={cat}/>
             })
           } 
-        </div>
+        </div> */}
       </div>
     );
 
